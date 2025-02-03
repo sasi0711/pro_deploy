d51db868c37e8b8cebae2f5a8a1ec6726fa92ddd
Sasikumar S <sasi071811@gmail.com>
2025-02-03 12:14:22 +0530

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ app.use(express.json())
 
 app.listen('1848', ()=> console.log('server running on 1848'))
 
-mongoose.connect('mongodb+srv://sasi0711:sasiKUMAR071811@mycluster.mr37s.mongodb.net/')
+mongoose.connect('mongodb+srv://sasi0711:sasi KUMAR071811@mycluster.mr37s.mongodb.net/')
     .then(()=> console.log('database connected..'))
     .catch((err) => console.log(err))
 
